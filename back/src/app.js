a8ab24b88e5e6ebf6493ba34fa4774741c2481ba
Heyden <nhm6947@gmail.com>
2022-04-13 16:35:15 +0900

    Fix: 사진 업로드시 request body size 조정

--- back/src/app.js
+++ back/src/app.js
@@ -15,8 +15,13 @@ app.use(cors());
 // express 기본 제공 middleware
 // express.json(): POST 등의 요청과 함께 오는 json형태의 데이터를 인식하고 핸들링할 수 있게 함.
 // express.urlencoded: 주로 Form submit 에 의해 만들어지는 URL-Encoded 형태의 데이터를 인식하고 핸들링할 수 있게 함.
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.json({
+  limit: "5mb"
+}));
+app.use(express.urlencoded({ 
+  limit: "5mb",
+  extended: false 
+}));
 
 // 기본 페이지
 app.get("/", (req, res) => {
