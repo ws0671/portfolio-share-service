e5d026d7f9cd045f6e485107ffcdd516e7794b6a
brilence75 <brilence75@gmail.com>
2022-04-15 10:44:15 +0900

    Feat : passport 인증 라우터연결

--- back/src/routers/userRouter.js
+++ back/src/routers/userRouter.js
@@ -4,7 +4,60 @@ import { login_required } from "../middlewares/login_required";
 import { upload } from "../middlewares/upload";
 import { userAuthService } from "../services/userService";
 
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth2").Strategy;
+
 const userAuthRouter = Router();
+
+
+// login이 최초로 성공했을 때만 호출되는 함수
+// done(null, user.id)로 세션을 초기화 한다.
+passport.serializeUser(function (user, done) {
+  done(null, user.id);
+});
+
+// 사용자가 페이지를 방문할 때마다 호출되는 함수
+// done(null, id)로 사용자의 정보를 각 request의 user 변수에 넣어준다.
+passport.deserializeUser(function (id, done) {
+  done(null, id);
+});
+
+// Google login 전략
+// 로그인 성공 시 callback으로 request, accessToken, refreshToken, profile 등이 나온다.
+// 해당 콜백 function에서 사용자가 누구인지 done(null, user) 형식으로 넣으면 된다.
+// 이 예시에서는 넘겨받은 profile을 전달하는 것으로 대체했다.
+passport.use(
+  new GoogleStrategy(
+      {
+          clientID: process.env.GOOGLE_CLIENT_ID,
+          clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+          callbackURL: "http://localhost:5000/auth/google/callback",
+          //passReqToCallback: true,
+          scope: ["email", "profile"],
+      },
+      function (request, accessToken, refreshToken, profile, done) {
+          console.log(profile);
+          console.log(accessToken);
+
+          return done(null, profile);
+      }
+  )
+);
+
+// google login 화면
+userAuthRouter.get(
+  "/auth/google",
+  passport.authenticate("google", { scope: ["email", "profile"] })
+);
+
+// google login 성공과 실패 리다이렉트
+userAuthRouter.get(
+  "/auth/google/callback",
+  passport.authenticate("google", {
+      successRedirect: "/",
+      failureRedirect: "/login",
+  })
+);
 
 userAuthRouter.post("/user/register", async function (req, res, next) {
   try {
