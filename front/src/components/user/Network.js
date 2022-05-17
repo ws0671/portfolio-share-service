9248dbae1d22cc3ddfc15af18482258c621d8936
ws0671 <ws0671@naver.com>
2022-05-17 20:50:45 +0900

    Feat: 검색기능

--- front/src/components/user/Network.js
+++ front/src/components/user/Network.js
@@ -2,7 +2,7 @@ import React, { useEffect, useContext, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { Container, Row } from "react-bootstrap";
 import styled from "styled-components";
-
+import { GoSearch } from "react-icons/go";
 import * as Api from "../../api";
 import UserCard from "./UserCard";
 import { UserStateContext } from "../../App";
@@ -14,6 +14,7 @@ function Network() {
   const [users, setUsers] = useState([]);
   const [searchWord, setSearchWord] = useState(null);
   const [data, setData] = useState(null);
+  const [page, setPage] = useState(1);
 
   useEffect(() => {
     // 만약 전역 상태의 user가 null이라면, 로그인 페이지로 이동함.
@@ -25,33 +26,58 @@ function Network() {
     Api.get("userlist").then((res) => setUsers(res.data));
   }, [userState, navigate]);
 
-  const getData = (word) => {
-    const res = Api.get(`user/search?name=${word}&page=1&perPage=10&sortField`);
-    console.log(res.data);
+  const getData = async (word) => {
+    const res = await Api.get(
+      `user/search?name=${word}&page=${page}&perPage=8&sortField`
+    );
+    setData(res.data);
   };
   const handleSubmit = (e) => {
     e.preventDefault();
     getData(searchWord);
+    console.log(data);
   };
   return (
     <Container fluid>
       <SearchBar>
         <form onSubmit={handleSubmit}>
+          <GoSearch style={{ position: "relative", left: "30px" }}></GoSearch>
           <input
             placeholder="유저이름으로 검색"
             onChange={(e) => setSearchWord(e.target.value)}
           ></input>
         </form>
       </SearchBar>
-      <Row xs="auto" className="justify-content-center">
-        {users.map((user) => (
-          <UserCard key={user.id} user={user} isNetwork />
-        ))}
-      </Row>
+      <Main>
+        <div className="cardContainer">
+          {data
+            ? data.map((user) => (
+                <UserCard key={user.id} user={user} isNetwork />
+              ))
+            : users.map((user) => (
+                <UserCard key={user.id} user={user} isNetwork />
+              ))}
+        </div>
+      </Main>
     </Container>
   );
 }
 const SearchBar = styled.div`
   text-align: center;
+  input {
+    width: 300px;
+    text-align: center;
+    padding: 0 40px;
+    border-radius: 20px;
+  }
+  margin-bottom: 50px;
+`;
+const Main = styled.main`
+  width: 80%;
+  margin: 0 auto;
+  .cardContainer {
+    flex-wrap: wrap;
+    display: flex;
+  }
 `;
 export default Network;
