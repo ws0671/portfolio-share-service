417ecc195557694910518fb7b9da68da9a006d69
saeleum <saeleumkim@gmail.com>
2022-05-18 20:50:11 +1000

    Update: Kakao API Key 변경

--- front/src/components/user/KakaoLogin.js
+++ front/src/components/user/KakaoLogin.js
@@ -3,7 +3,7 @@ import styles from "../../styles/mystyle.module.css";
 // import axios from "axios";
 
 function KakaoLogin() {
-  const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id="3e248ae78cfb8d9f2b859a45232a7e01"&redirect_uri="http://localhost:3000/oauth/callback/kakao"&response_type=code`;
+  const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${process.env.KAKAO_CLIENT_ID}&redirect_uri=${process.env.KAKAO_REDIRECT_URI}&response_type=code`;
 
   return (
     <>
