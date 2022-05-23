0f556db2350f0e8ec964f68a7c3e5401864531db
Heyden <nhm6947@gmail.com>
2022-05-23 19:14:08 +0900

    Feat: 댓글 Router, Model ap 추가 (app, db/index)

--- back/src/app.js
+++ back/src/app.js
@@ -5,6 +5,7 @@ import { certificateRouter } from "./routers/certificateRouter";
 import { educationRouter } from "./routers/educationRouter";
 import { errorMiddleware } from "./middlewares/errorMiddleware";
 import { projectRouter } from "./routers/projectRouter";
+import { commentRouter } from "./routers/commentRouter";
 import { userAuthRouter } from "./routers/userRouter";
 import { googleAuthRouter } from "./routers/googleAuthRouter";
 
@@ -41,6 +42,7 @@ app.use(awardRouter);
 app.use(certificateRouter);
 app.use(educationRouter);
 app.use(projectRouter);
+app.use(commentRouter);
 
 // 순서 중요 (router 에서 next() 시 아래의 에러 핸들링  middleware로 전달됨)
 app.use(errorMiddleware);
